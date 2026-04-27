085f50f202086efa99721ce7b85ec8ec1f90bb78
ana-zevedo <a.candida@escolar.ifrn.edu.br>
2026-04-27 09:44:40 -0300

    Update pygameJanela.py

--- pygameJanela.py
+++ pygameJanela.py
@@ -8,7 +8,7 @@ largura = 640
 altura = 480
 
 tela = pygame. display.set_mode((largura, altura))
-pygame.display.set_caption('jogo')
+pygame.display.set_caption(''Core-sync: Protocolo de Sobrecarga')
 
 while True:
     for event in pygame.event.get():
