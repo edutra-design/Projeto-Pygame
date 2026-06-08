4805cc721f81c615bda54fed831a1e581ad086f9
juliadutrafernandes <juliadutrafernandes70@gmail.com>
2026-06-08 09:07:30 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -1,37 +1,20 @@
+#personagem.py
 import pygame
 
 class Entidade:
 
-    def __init__(self, x, y, tamanho, caminho_imagem):
+    def _init_(self, x, y, tamanho, cor):
         self.x = x
         self.y = y
         self.tamanho = tamanho
-      try: 
-        imagem_original = pygame.image.load(caminho_imagem).convert_alpha()
-        self.image = pygame.transform.scale(image_original, (self.tamanho, self.tamanho))
-except FileNotFoundError:
-    print(f"Aviso: Imagem '{caminho_imagem}' não encontrada.")
-    self.imagem = pygame.Surface((self.tamanho, self.tamanho))
-    self.imagem.fill((255, 0, 255))
+        self.cor = cor
+
 
 class Jogador(Entidade):
 
-    def __init__(self, x, y, tamanho):
-        super().__init__(x, y, tamanho, "assets/sprites/nucleos_neon.png")
+    def _init_(self, x, y, tamanho):
+        super()._init_(x, y, tamanho, (0, 255, 255))
 
-        self.vel_x = 0
-        self.vel_y = 0
-        self.velocidade_deslize = 8
-        self.deslizando = False
-      
-    def comando_mover(self, dx, dy):
-      if not self.deslizando:
-        self.vel_x = dx * self.velocidade_deslize
-        self.vel_y = dy * self.velocidade_deslize
-        self.deslizando = True
-
-    def update(self, paredes):
-      self.x += self.vel_x # Ana: parei aqui
     def desenhar(self, tela):
 
         pygame.draw.rect(
