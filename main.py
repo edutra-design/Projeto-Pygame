f691a1656874d17fece1997e8844be4e7aa8869f
Ana Azevedo <a.candida@escolar.ifrn.edu.br>
2026-06-08 10:06:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #main.py
 import pygame
 import sys
+import os
 
 from personagem import Jogador
 from inimigos import Inimigo
@@ -12,6 +13,19 @@ ALTURA = 600
 
 TELA = pygame.display.set_mode((LARGURA, ALTURA))
 
+cor_texto = (255,255,255)
+cor_destaque = (0,255,255)
+
+fonte_titulo = pygame.font.SysFont("Arial", 60, bold=True)
+font_opcoes = pygame.font.SysFont("Arial", 40)
+
+estado_atual = "menu"
+def desenhar_opcao(texto, fonte, cor, centro_y):
+    superficie = fonte.render(texto, True, cor)
+    retangulo = superficie.get_rect(center=(LARGURA // 2, centro_y))
+    TELA.blit(superficie, retangulo)
+    return retangulo 
+    
 jogador = Jogador(100, 100, 40)
 inimigo = Inimigo(300, 300, 40)
 
