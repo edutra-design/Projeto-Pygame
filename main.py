122832b3f9e36b5572fd752bbfe6aec2e98c7c2e
juliadutrafernandes <juliadutrafernandes70@gmail.com>
2026-06-08 09:06:30 -0300

    Initialize Pygame with player and enemy setup

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+#main.py
+import pygame
+import sys
 
+from personagem import Jogador
+from inimigos import Inimigo
+
+pygame.init()
+
+LARGURA = 800
+ALTURA = 600
+
+TELA = pygame.display.set_mode((LARGURA, ALTURA))
+
+jogador = Jogador(100, 100, 40)
+inimigo = Inimigo(300, 300, 40)
+
+rodando = True
+
+while rodando:
+
+    for evento in pygame.event.get():
+
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+    TELA.fill((10, 10, 25))
+
+    jogador.desenhar(TELA)
+    inimigo.desenhar(TELA)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
