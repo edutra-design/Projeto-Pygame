964eda59fe07ce1a82f5ae9a9da6265a7d5c91b8
Ana Azevedo <a.candida@escolar.ifrn.edu.br>
2026-06-15 09:16:14 -0300

    Update efeitos.py

--- efeitos.py
+++ efeitos.py
@@ -4,7 +4,7 @@ from personagem import Entidade
 
 class Efeito(Entidade):
 
-    def _init_(self, x, y, tamanho):
+    def __init__(self, x, y, tamanho):
         super()._init_(x, y, tamanho, (255, 255, 0))
 
     def desenhar(self, tela):
