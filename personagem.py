5270ee7f667d082a6d838ef20dedebda53a3df31
juliadutrafernandes <juliadutrafernandes70@gmail.com>
2026-06-18 16:50:06 -0300

    Add movement and sprite handling to Jogador class

--- personagem.py
+++ personagem.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Entidade:
 
     def __init__(self, x, y, tamanho, cor):
@@ -10,13 +11,42 @@ class Entidade:
 
 
 class Jogador(Entidade):
+
     def __init__(self, x, y, tamanho):
-     
         super().__init__(x, y, tamanho, (0, 255, 255))
 
+        self.velocidade = 5
+
+        self.sprite = pygame.image.load(
+            "assets/Mad_Mask.webp"
+        ).convert_alpha()
+
+        self.sprite = pygame.transform.scale(
+            self.sprite,
+            (tamanho, tamanho)
+        )
+
+    def mover(self):
+
+        teclas = pygame.key.get_pressed()
+
+        if teclas[pygame.K_w]:
+            self.y -= self.velocidade
+
+        if teclas[pygame.K_s]:
+            self.y += self.velocidade
+
+        if teclas[pygame.K_a]:
+            self.x -= self.velocidade
+
+        if teclas[pygame.K_d]:
+            self.x += self.velocidade
+
     def desenhar(self, tela):
-        pygame.draw.rect(
-            tela,
-            self.cor,
-            (self.x, self.y, self.tamanho, self.tamanho)
+
+        self.mover()  # movimenta antes de desenhar
+
+        tela.blit(
+            self.sprite,
+            (self.x, self.y)
         )
